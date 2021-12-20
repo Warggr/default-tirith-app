874704281f9afbe1457c3a289b12ffeff8e19fc9
Pierre Ballif <pierre.ballif@tum.de>
2021-12-20 17:37:14 -0500

    Add path for health checks

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+# DO NOT remove this route; it is used internally by the load balancer to check whether a target is healthy.
+@app.route("/tirith-health-checks")
+def health_check():
+    return "Healthy!"
+
 if __name__ == "__main__":
     app.run()
